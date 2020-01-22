44f7d21cb5a8f913d9fa800da20a077cc5e22d60
leeroyc710 <leeroy.chiweshe@academytrainee.com>
2020-01-22 09:00:03 +0000

    fixes

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -2,11 +2,15 @@ from flask import render_template, redirect, url_for, request
 from application import app
 from application.forms import DareForm, SubmitField
 import requests, json
+import random
 
 #------------------       Main_Service Handling Service3             --------------------------
 
 @app.route('/', methods=['GET','POST'])
 def generator():
+    list=["text-muted", "text-primary", "text-success", "text-info", "text-warning", "text-danger", "text-secondary", "text-dark", "text-light"]
+    
+    choice=random.choice(list)
 
     form = DareForm()
     payload={'':''}
@@ -14,7 +18,7 @@ def generator():
     if request.method =='POST':
         
        response = requests.post("http://service3:5003/").json()
-       return render_template('home.html', form=form, data=response)
+       return render_template('home.html', form=form, data=response, choice=choice)
    
     if request.method=='GET':
        return render_template('dare.html', title= 'Dare', form=form)
