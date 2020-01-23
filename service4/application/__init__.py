ed046b8eea81ed549d42c9727a90cc3ead73dc63
Ubuntu <ubuntu@ip-172-31-46-235.eu-west-2.compute.internal>
2020-01-23 15:38:36 +0000

    added aws-x-ray config

--- service4/application/__init__.py
+++ service4/application/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'yes'
-#--------------------code for modelling databases check models.py-------------------------------------
+
+#___________________________________________________________x-ray configuration_____________________________________________
+
+xray_recorder.configure(service='myapp')
+XRayMiddleware(app, xray_recorder)
+
+#_______________________________________code for app.config will go here for the environment__________________________________
 
 from application import routes
